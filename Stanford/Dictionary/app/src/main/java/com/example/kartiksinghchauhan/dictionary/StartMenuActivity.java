01efeef008d8960bf87c6851862fc74db10983f9
Kartik Singh Chauhan <lkchauhan.725@gmail.com>
2018-03-30 23:41:51 +0530

    added login activity

--- Stanford/Dictionary/app/src/main/java/com/example/kartiksinghchauhan/dictionary/StartMenuActivity.java
+++ Stanford/Dictionary/app/src/main/java/com/example/kartiksinghchauhan/dictionary/StartMenuActivity.java
@@ -1,20 +1,58 @@
 package com.example.kartiksinghchauhan.dictionary;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import stanford.androidlib.SimpleActivity;
 
 public class StartMenuActivity extends SimpleActivity {
+
+    Button button;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
     private static final int REQ_CODE_ADD_WORD = 1234;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
         setTraceLifecycle(true);
+        button = (Button)findViewById(R.id.logout);
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        TextView welcome = (TextView)findViewById(R.id.welcome);
+        welcome.setText("Welcome " + user.getEmail());
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(StartMenuActivity.this,SignUpActivity.class));
+                }
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
     }
 
     public void playTheGameClick(View view) {
