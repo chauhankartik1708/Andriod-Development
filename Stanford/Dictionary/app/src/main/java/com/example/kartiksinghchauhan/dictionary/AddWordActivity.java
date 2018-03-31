76d2626c74df68779dfe8c1965f4033dadb1a656
Kartik Singh Chauhan <lkchauhan.725@gmail.com>
2018-03-31 13:47:44 +0530

    add word will write in firebase from now

--- Stanford/Dictionary/app/src/main/java/com/example/kartiksinghchauhan/dictionary/AddWordActivity.java
+++ Stanford/Dictionary/app/src/main/java/com/example/kartiksinghchauhan/dictionary/AddWordActivity.java
@@ -1,30 +1,53 @@
 package com.example.kartiksinghchauhan.dictionary;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.Toast;
 
-import java.io.PrintStream;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import stanford.androidlib.SimpleActivity;
 
 public class AddWordActivity extends SimpleActivity {
-
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_word);
         setTraceLifecycle(true);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(AddWordActivity.this,StartMenuActivity.class));
+                }
+            }
+        };
     }
 
     public void addThisWordClick(View view) {
         String newword = $ET(R.id.new_word).getText().toString();
         String newdefn = $ET(R.id.new_defn).getText().toString();
         if (!newword.isEmpty() && !newdefn.isEmpty()) {
-            PrintStream output = new PrintStream(openFileOutput("added_words.txt", MODE_PRIVATE | MODE_APPEND));
-            output.println(newword + "@" + newdefn);
-            output.close();
+            FirebaseUser user = mAuth.getCurrentUser();
+            String word = newword.trim();
+            String defn = newdefn.toString().trim();
+            databaseReference = database.getReference("Users").child(user.getUid()).child(word);
+            databaseReference.child("defn").setValue(defn);
             try {
                 Intent goBack = new Intent();
                 goBack.putExtra("newword", newword);
